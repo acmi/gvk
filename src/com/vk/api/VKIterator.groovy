80b1fc3285b1746fea5e622337fd3f8ae7e9d7cd
Alexey Sazonov <acmi831@gmail.com>
2013-05-19 13:13:34 +0400

    VKIterator

---
+++ src/com/vk/api/VKIterator.groovy
@@ -0,0 +1,55 @@
+package com.vk.api
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+abstract class VKIterator<T> implements Iterator<T>{
+    protected final VKEngineGPath engine
+    protected final int offset
+
+    private int count = -1
+    private int received
+    protected final Queue<T> buffer = new LinkedList<T>()
+
+    public VKIterator(VKEngineGPath engine, int offset) {
+        this.engine = engine
+        this.offset = offset
+    }
+
+    protected int getReceived() {
+        return received;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (count == -1) {
+            try {
+                count = getCount();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return offset + received < count
+    }
+
+    @Override
+    public T next() {
+        if (buffer.isEmpty())
+            try {
+                fillBuffer()
+            } catch (Exception e) {
+                throw new RuntimeException(e)
+            }
+        received++
+        return buffer.poll()
+    }
+
+    protected abstract int getCount() throws Exception
+
+    protected abstract void fillBuffer() throws Exception
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException()
+    }
+}
